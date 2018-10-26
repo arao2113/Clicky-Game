d640c0d5431e363edd2576027129809298f8ac4e
Ara P <arao2113@mail.com>
2018-10-25 19:56:46 -0700

    Updates

---
+++ precache-manifest.2c621361c67cbb89e6ac5b0ae6a9607b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "131d2a71c1669f685d8c",
+    "url": "/clicky-game/static/js/main.131d2a71.chunk.js"
+  },
+  {
+    "revision": "bc2a64db4ab27c5c636d",
+    "url": "/clicky-game/static/js/1.bc2a64db.chunk.js"
+  },
+  {
+    "revision": "131d2a71c1669f685d8c",
+    "url": "/clicky-game/static/css/main.e2dba0a2.chunk.css"
+  },
+  {
+    "revision": "ab6ab406ad2d95e899c9b5be96d3e8de",
+    "url": "/clicky-game/index.html"
+  }
+];
